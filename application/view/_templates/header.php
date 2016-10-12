73891785cb41d9da925ed10769b0739f6325c340
clashshot <erfan.atg@gmail.com>
2016-10-12 15:22:56 +0200

    Fixed header for blog controller

--- application/view/_templates/header.php
+++ application/view/_templates/header.php
@@ -23,7 +23,7 @@
 <body>
     <?php if (View::checkForActiveController($filename, "blog")) { ?>
         <nav class="navbar navbar-fixed-top navbar-inverse">
-            <div class="container-fluid">
+            <div class="container">
                 <div class="navbar-header">
                     <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
                         <span class="sr-only">Toggle navigation</span>
