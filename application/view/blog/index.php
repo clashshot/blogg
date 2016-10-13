4362e2db8c7092e55f4ae1f6cb26db220c97ab97
Peychas <Nian_wow@live.se>
2016-10-13 15:15:07 +0200

    layout with social media added

--- application/view/blog/index.php
+++ application/view/blog/index.php
@@ -40,10 +40,20 @@
         </div>
         <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
             <div class="well">
-                <div class="profile-userpic">
+                <div class="profile-userpic text-center">
                     <img src="<?=$this->user->user_avatar_link?>">
                 </div>
-                <?= $this->blog->description ?>
+                <div class="text-center">
+                    <h2><?=$this->user->user_name ?></h2>
+                </div>
+                <h4 class="text-center"><?= $this->blog->description ?></h4>
+                <div class="text-center">
+                    <ul class="social-network social-circle">
+                        <li><a href="#" class="icoFacebook" title="Facebook"><i class="fa fa-facebook"></i></a></li>
+                        <li><a href="#" class="icoTwitter" title="Twitter"><i class="fa fa-twitter"></i></a></li>
+                        <li><a href="#" class="icoGoogle" title="Google +"><i class="fa fa-google-plus"></i></a></li>
+                    </ul>
+                </div>
             </div>
         </div>
     </div>
