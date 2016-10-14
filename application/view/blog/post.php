b5485b454054645d845efb770e7572e5abad914c
MrBulldozer <estiv97@gmail.com>
2016-10-14 14:35:27 +0200

    Fixed comments
    you can't send an empty comment now

--- application/view/blog/post.php
+++ application/view/blog/post.php
@@ -1,53 +1,63 @@
 <?php
-    function renderComments($comments){
-        if(!empty($comments)) {
-            foreach ($comments as $comment) {
-                $comment->user_avatar_link = (Config::get('USE_GRAVATAR') ? AvatarModel::getGravatarLinkByEmail($comment->user_email) : AvatarModel::getPublicAvatarFilePathOfUser($comment->user_has_avatar, $comment->user_id));
-                ?>
-                <div class="media">
-                    <div class="media-left">
-                        <?php if (isset($comment->user_avatar_link)) { ?>
-                            <img src="<?=$comment->user_avatar_link?>"/>
-                        <?php } ?>
-                    </div>
-                    <div class="media-body">
-                        <h4 class="medua-heading">
-                            <?=$comment->user_name?> <small><i>Kommenterades <?=$comment->created ?></i></small>
-                        </h4>
-                        <p><?=$comment->comment ?></p>
-                        <?php
-                        renderComments($comment->subComments);
-                        ?>
-                    </div>
+function renderComments($comments)
+{
+    if (!empty($comments)) {
+        foreach ($comments as $comment) {
+            $comment->user_avatar_link = (Config::get('USE_GRAVATAR') ? AvatarModel::getGravatarLinkByEmail($comment->user_email) : AvatarModel::getPublicAvatarFilePathOfUser($comment->user_has_avatar, $comment->user_id));
+            ?>
+            <div data-comment="<?= $comment->id ?>" class="media">
+                <div class="media-left">
+                    <?php if (isset($comment->user_avatar_link)) { ?>
+                        <img src="<?= $comment->user_avatar_link ?>"/>
+                    <?php } ?>
                 </div>
-                <?php
-            }
+                <div class="media-body">
+                    <h4 class="medua-heading">
+                        <?= $comment->user_name ?>
+                        <small><i>Kommenterades <?= $comment->created ?></i></small>
+                    </h4>
+                    <p><?= $comment->comment ?></p>
+                    <?php
+                    renderComments($comment->subComments);
+                    ?>
+                </div>
+            </div>
+            <?php
         }
     }
+}
+
 ?>
 
 <div class="container">
     <div class="row">
         <div class="col-md-8">
             <div class="well">
-            <h2 class="text-center"><?=$this->post->title?></h2>
-            <?=$this->post->content?>
+                <h2 class="text-center"><?= $this->post->title ?></h2>
+                <?= $this->post->content ?>
             </div>
         </div>
+        <?php
+        include "sidebar.php";
+        ?>
     </div>
     <div class="row">
-        <div class="col-md-12 well">
-            <div class="col-md-12">
-                <form method="post" action="<?=Config::get("URL") . $this->blog->slug . "/comment/" . $this->post->slug ?>">
-                    <textarea type="text" class="form-control" name="comment" placeholder="¯\_(ツ)_/¯"></textarea>
-                    <br/>
-                    <input type="submit" class="btn-primary"/>
-                </form>
-            </div>
-            <div class="col-md-12">
-                <?php
-                renderComments($this->comments);
-                ?>
+        <div class="col-md-12">
+            <div class="well" style="overflow: auto">
+                <div class="col-md-12">
+                    <form method="post"
+                          action="<?= Config::get("URL") . $this->blog->slug . "/comment/" . $this->post->slug ?>">
+                        <textarea type="text" class="form-control" name="comment" placeholder="Write a comment..."
+                                  required></textarea>
+                        <br/>
+                        <input type="submit" class="btn-primary"/>
+                    </form>
+                </div>
+                <div class="col-md-12">
+                    <?php
+                    renderComments($this->comments);
+                    ?>
+                </div>
             </div>
         </div>
     </div>
