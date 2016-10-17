658d87e180ba904a5b43a11d55054fd6cc21e6b7
rickfo97 <rickfo_97@hotmail.com>
2016-10-17 12:23:19 +0200

    Fix?

--- application/model/CategoryModel.php
+++ application/model/CategoryModel.php
@@ -13,4 +13,21 @@ class CategoryModel
         return $sql->fetchAll();
     }
 
+    public static function addCategory($blogid){
+        $database = DatabaseFactory::getFactory()->getConnection();
+        $query = $database->prepare("INSERT INTO Category(blog_id, `name`) VALUES(:blog, :name)");
+        return $query->execute(array(
+            ':blog' => $blogid,
+            ':name' => Filter::XSSFilter(Request::post('name'))
+        ));
+    }
+
+    public static function removeCategory($blogid){
+        $database = DatabaseFactory::getFactory()->getConnection();
+        $query = $database->prepare("DELETE FROM Category WHERE id = :id AND blog_id = :blog");
+        return $query->execute(array(
+            ':id' => Request::post('category'),
+            ':blog' => $blogid
+        ));
+    }
 }
