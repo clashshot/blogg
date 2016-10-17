d1c6fee9abc5753e22b975ffaba3ba2fd6d26364
Jakob Sandstedt <sandstedt_95@hotmail.com>
2016-10-17 12:12:18 +0200

    favorite

---
+++ application/model/FavoriteModel.php
@@ -0,0 +1,42 @@
+<?php
+
+
+class FavoriteModel
+{
+    public static function addfavorite()
+    {
+        $database = DatabaseFactory::getFactory()->getConnection();
+
+        $query = $database->prepare("INSERT INTO Favorite(user_id, post_id) VALUES (:id, :postid)");
+        return $query->execute(array(
+            ':id' => Session::get('user_id'),
+            ':postid' => Request::post('postid')
+        ));
+    }
+
+    public static function removefavorite()
+    {
+        $database = DatabaseFactory::getFactory()->getConnection();
+
+        $query = $database->prepare("DELETE FROM Favorite WHERE user_id = :id, post_id = :postid ");
+        return $query->execute(array(
+            ':id' => Session::get('user_id'),
+            ':postid' => Request::post('postid')
+        ));
+    }
+    public static function favoritelist($page = 0, $post_per_page = 10){
+
+        $database = DatabaseFactory::getFactory()->getConnection();
+        $query = $database->prepare("SELECT * FROM Favorite LEFT JOIN Post ON Favorite.post_id = Post.id WHERE Favorite.user_id = :userid LIMIT ($page * $post_per_page), $post_per_page");
+        $query->execute(array(
+            ':userid' => Session::get('user_id')
+        ));
+        if ($query->rowCount()> 0){
+            $posts = array();
+            while($post = $query->fetchObject()){
+                $posts[] = $post;
+            }
+            return $posts;
+        }
+    }
+}
