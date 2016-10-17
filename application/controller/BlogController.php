af61e83163ea544cd3d786f77dcf173cd6590fe3
rickfo97 <rickfo_97@hotmail.com>
2016-10-17 10:47:00 +0200

    merge correct

--- application/controller/BlogController.php
+++ application/controller/BlogController.php
@@ -56,75 +56,6 @@ class BlogController extends Controller
 
     public function manage($blogid, $method = 'index', $postslug = '')
     {
-<<<<<<< HEAD
-        switch (strtolower($method)) {
-            case 'index':
-                $this->View->render('manage/index', array(
-                    'blog' => BlogModel::getBlog($blogid),
-                    'posts' => BlogModel::getPosts($blogid, Request::get("page"), 10),
-                    'paginate' => new Paginate("Post WHERE blog_id = :blog_id", [':blog_id' => $blogid], 10)
-                ));
-                break;
-            case 'addpost':
-                $this->View->render('manage/addpost', array(
-                    'blog' => BlogModel::getBlog($blogid)
-                ));
-                break;
-            case 'addpost_action':
-                if (BlogModel::addpost($blogid)) {
-                    Session::add('feedback_positive', 'Inlägg skapades');
-                    Redirect::to(BlogModel::getBlog($blogid)->slug . '/manage/index');
-                } else {
-                    Session::add('feedback_negative', 'Inlägg kunde ej skapas, försök igen.');
-                    Redirect::to(BlogModel::getBlog($blogid)->slug . '/manage/addpost');
-                }
-                break;
-            case 'editpost':
-                $post = BlogModel::getpost($blogid, $postslug);
-                $this->View->render('manage/editpost', array('post' => $post));
-                break;
-            case 'deletepost':
-                if (UserModel::getEditPermission($blogid) >= 3) {
-                    BlogModel::deletepost($blogid, $postslug);
-                    Redirect::to(BlogModel::getBlog($blogid)->slug . '/manage');
-                } else {
-                    Redirect::to(BlogModel::getBlog($blogid)->slug . '/manage');
-                }
-                break;
-            case 'update':
-                $this->View->render('manage/editblog', array(
-                    "blog" => BlogModel::getBlog($blogid)
-                ));
-                break;
-            case 'blog_update':
-                BlogModel::blog_update($blogid);
-                Redirect::to(BlogModel::getBlog($blogid)->slug . '/manage');
-                break;
-            case 'history':
-                echo 'history';
-                break;
-            case 'mods':
-                $this->View->render('manage/mods', array(
-                    'blog' => BlogModel::getBlog($blogid),
-                    'mods' => BlogModel::getMods($blogid)
-                ));
-                break;
-            case 'addmod_action':
-                if (BlogModel::addMod($blogid)) {
-                    Redirect::to(BlogModel::getBlog($blogid)->slug . '/manage/mods');
-                } else {
-                    Redirect::to(BlogModel::getBlog($blogid)->slug . '/manage/mods');
-                }
-                break;
-            case 'category':
-                $this->View->render('manage/category', array(
-                    'blog' => BlogModel::getBlog($blogid),
-                    'category' => CategoryModel::showCategory($blogid),
-                    'paginate' => new Paginate("Category WHERE blog_id = :blog_id", [':blog_id' => $blogid], 10)
-                ));
-                break;
-            case 'addcategory':
-=======
         if (UserModel::getEditPermission($blogid)) {
             switch (strtolower($method)) {
                 case 'index':
@@ -165,6 +96,10 @@ class BlogController extends Controller
                         "blog" => BlogModel::getBlog($blogid)
                     ));
                     break;
+                case 'blog_update':
+                    BlogModel::blog_update($blogid);
+                    Redirect::to(BlogModel::getBlog($blogid)->slug . '/manage');
+                    break;
                 case 'history':
                     echo 'history';
                     break;
@@ -193,8 +128,6 @@ class BlogController extends Controller
                     ));
                     break;
                 case 'addcategory':
->>>>>>> 1ec8380ca9114ec76fa8f5fb579f367d6c9b9df7
-
                     break;
                 case 'remove':
                     break;
