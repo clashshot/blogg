21df58312e7928711291ddd8feef00a5580cca50
Peychas <Nian_wow@live.se>
2016-10-21 13:45:58 +0200

    page design fix

--- application/view/page/index.php
+++ application/view/page/index.php
@@ -6,7 +6,6 @@ $bbcode = new Golonka\BBCode\BBCodeParser;
         <div class="col-md-8">
             <div class="well">
                 <h1 class="text-center"><?= $this->page->title;?></h1>
-                <p class="text-center"><?= $this->user->user_name?></p>
                 <div><?= $bbcode->parse($this->page->content)?></div>
             </div>
         </div>
