e65da1590bfe18137b67a162c2369c447a094f7f
rickfo97 <rickfo_97@hotmail.com>
2016-10-21 12:34:20 +0200

    Dahboard fix

--- application/controller/DashboardController.php
+++ application/controller/DashboardController.php
@@ -32,9 +32,9 @@ class DashboardController extends Controller
 
     public function favorite() {
         $this->View->render('dashboard/favorite', array(
-            'favoritelist' => FavoriteModel::favoritelist()
+            'posts' => FavoriteModel::favoritelist(),
+            'paginate' => new Paginate("Favorite WHERE Favorite.user_id = :userid", array('userid' => Session::get('user_id')), 10)
         ));
-
     }
 
     public function blog_create(){
