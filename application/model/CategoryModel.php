64f95e62188ebdbad11e8e28f1f5a0c28b3e41e4
rickfo97 <rickfo_97@hotmail.com>
2016-10-24 10:38:29 +0200

    No new category when exists

--- application/model/CategoryModel.php
+++ application/model/CategoryModel.php
@@ -5,7 +5,6 @@ class CategoryModel
     public static function showCategory($blogid, $exclude = null, $page = 0, $IPP = 10)
     {
         $database = DatabaseFactory::getFactory()->getConnection();
-
         $sql = 'SELECT * FROM Category WHERE';
         if (isset($exclude)){
             $sql .= ' id != :exclude AND';
@@ -29,19 +28,13 @@ class CategoryModel
         return $sql->fetchAll();
     }
 
-    public static function showCatexistsinPost($blogid){
-        $database = DatabaseFactory::getFactory()->getConnection();
-        // Show categories -> if category id exists in post
-        $query = $database->prepare('SELECT c.* FROM Category AS c WHERE c.blog_id = :blogid AND EXISTS (SELECT 1 FROM Post AS p WHERE p.category_id = c.id)');
-        $query->execute(array(
-            ':blogid' => $blogid
-        ));
-        return $query->fetchAll();
-    }
-
     public static function addCategory($blogid){
         $name = Request::post('name');
         $baseslug = BlogModel::slugify($name);
+
+        if(self::categoryexists($blogid, $name)){
+            return true;
+        }
 
         $slug = $baseslug;
         for ($i = 0; $i < 5; $i++) {
@@ -96,34 +89,26 @@ class CategoryModel
         ));
     }
 
+    private static function categoryexists($blogid, $name){
+        $database = DatabaseFactory::getFactory()->getConnection();
+        $query = $database->prepare("SELECT *FROM Category WHERE blog_id = :blog AND name = :name");
+        $query->execute(array(
+            ':blog' => $blogid,
+            ':name' => $name
+        ));
+
+        if($query->rowCount() > 0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
     public static function getnamebyid($tablename, $columnname, $value){
         $database = DatabaseFactory::getFactory()->getConnection();
-
-        $query = $database->prepare("SELECT * FROM $tablename WHERE $columnname = '$value'");
+        $query = $database->prepare("SELECT * FROM $tablename WHERE $columnname = $value");
         $query->execute();
 
         return $query->fetchObject();
     }
-
-    public static function catpage($blogid, $catslug){
-        $database = DatabaseFactory::getFactory()->getConnection();
-
-        $catid = $database->prepare("SELECT id FROM Category WHERE slug = :catslug");
-        $catid->execute(array(':catslug'=>$catslug));
-        $cat = $catid->fetchObject();
-
-        $query = $database->prepare("SELECT * FROM Post WHERE blog_id = :blogid AND category_id = :catid");
-        $query->execute(array(
-            ':blogid' => $blogid,
-            ':catid' => $cat->id
-        ));
-        $postarray = array();
-        while ($post = $query->fetchObject()) {
-            $post->comments = CommentModel::getCommentAmount($post->id);
-            $post->likes = BlogModel::getPostLikes($post->id);
-            $postarray[] = $post;
-        }
-        return $postarray;
-    }
-
 }
