2f3605e510dbf3c16b79a278743cff454958b49a
Jakob Sandstedt <sandstedt_95@hotmail.com>
2016-10-25 10:00:14 +0200

    about2.1

--- application/view/_templates/aboutus.php
+++ application/view/_templates/aboutus.php
@@ -2,7 +2,14 @@
     <div class="panel panel-default">
         <div class="panel-heading">Om oss:</div>
         <div class="panel-body">
-
+        <p>Vi är en grupp på sex stycken personer som går fjärde året teknik på IT-gymnasiet i Skövde. Vi har fått som uppgift att skapa en blogghanterare.  </p>
+            <h1>Gruppmedlemmar:</h1>
+            <p>Jakob Sandstedt</p>
+            <p>Hannes Esplund</p>
+            <p>Pontus Astanius</P>
+            <p>Kevin Af Hedsand</p>
+            <p>Rickardh Forslund</p>
+            <p>Erfan Atagozli</p>
 
         </div>
     </div>
