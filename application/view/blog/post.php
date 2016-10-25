5472f1f19f839878ca42552801f29243b949d529
MrBulldozer <estiv97@gmail.com>
2016-10-25 10:42:05 +0200

    Textarea Maxlength
    A simple and crude fix to limit the comment length

--- application/view/blog/post.php
+++ application/view/blog/post.php
@@ -93,7 +93,7 @@ function renderComments($post, $blog_id, $blogslug, $postslug, $comments, $depth
                             if (ReportModel::reportexists(Session::get('user_id'), 1, $comment->id)) {
                                 echo 'Rapporterad';
                             } else {
-                                echo '<a onclick="report(this,'.$comment->id.', 1, prompt(\'Anledning till rapportering\', \'\'))" class="btn btn-xs btn-danger glyphicon glyphicon-flag"></a>';
+                                echo '<a onclick="report(this,' . $comment->id . ', 1, prompt(\'Anledning till rapportering\', \'\'))" class="btn btn-xs btn-danger glyphicon glyphicon-flag"></a>';
                             } // End Report Comment
                             ?>
                         <?php } ?>
@@ -110,7 +110,7 @@ function renderComments($post, $blog_id, $blogslug, $postslug, $comments, $depth
                                               action="<?= Config::get("URL") . $blogslug . "/update_comment/" . $postslug ?>">
                                             <input type="hidden" name="comment_id" value="<?= $comment->id ?>"/>
                                             <br/>
-                                            <textarea type="text" name="comment"
+                                            <textarea maxlength=255 name="comment"
                                                       class="form-control"><?= $comment->comment ?></textarea>
                                             <br/>
                                             <input type="submit" class="btn btn-primary btn-sm" value="Ändra"/>
@@ -131,7 +131,7 @@ function renderComments($post, $blog_id, $blogslug, $postslug, $comments, $depth
                                                    value="<?= $comment->comment_id ?>"/>
                                         <?php } ?>
                                         <br/>
-                                        <textarea type="text" name="comment" class="form-control"></textarea>
+                                        <textarea maxlength=255 name="comment" class="form-control"></textarea>
                                         <br/>
                                         <input type="submit" class="btn btn-primary btn-sm" value="Skicka"/>
                                     </form>
@@ -140,8 +140,8 @@ function renderComments($post, $blog_id, $blogslug, $postslug, $comments, $depth
                         </div>
                         <?php
                     }
-                    if($depth <= 7){
-                        renderComments($post, $blog_id, $blogslug, $postslug, $comment->subComments, $depth +1);
+                    if ($depth <= 7) {
+                        renderComments($post, $blog_id, $blogslug, $postslug, $comment->subComments, $depth + 1);
                     }
                     ?>
                 </div>
@@ -209,7 +209,7 @@ $bbcode = new Golonka\BBCode\BBCodeParser;
                 <div class="col-md-12">
                     <form method="post"
                           action="<?= Config::get("URL") . $this->blog->slug . "/comment/" . $this->post->slug ?>">
-                        <textarea type="text" class="form-control" name="comment"
+                        <textarea maxlength=255 class="form-control" name="comment"
                                   placeholder="<?php if ($this->post->allow_comments == 1)
                                       echo "Skriv en kommentar...";
                                   else
