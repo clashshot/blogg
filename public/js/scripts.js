79fae9522308e1ce8645622b09780361afe5b5bc
clashshot <erfan.atg@gmail.com>
2016-10-28 13:29:27 +0200

    Update scripts.js

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -3,11 +3,11 @@ var recaptcha2;
 var myCallBack = function() {
 
     recaptcha1 = grecaptcha.render('recaptcha1', {
-        'sitekey' : '6Ldk-wgUAAAAAPGDPYPxtgopZxFfDIDmqvafCJ36'
+        'sitekey' : 'SITEKEYHERE'
     });
 
     recaptcha2 = grecaptcha.render('recaptcha2', {
-        'sitekey' : '6Ldk-wgUAAAAAPGDPYPxtgopZxFfDIDmqvafCJ36'
+        'sitekey' : 'SITEKEYHERE'
     });
 };
 
